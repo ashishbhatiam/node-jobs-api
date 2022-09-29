bdf0937d4aff3e226fc3187ef77ec1dc55826b7e
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-09-30 00:05:04 +0530

    added basic create user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
-const register = (req, res) => {
-  res.send('Register User!')
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body })
+  res.status(StatusCodes.CREATED).json(user)
 }
 
 const login = (req, res) => {
