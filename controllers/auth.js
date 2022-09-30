e6365faaaeb86569b0fca287aed6c0edd26024a6
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-10-01 00:53:03 +0530

    added create JWT schema custom instance method for User

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,9 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
-  const token = jwt.sign(
-    { userId: user._id, name: user.name },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_OPT_EXPIRES_IN
-    }
-  )
+  const token = user.createJwt()
   res
     .status(StatusCodes.CREATED)
     .json({ user: { id: user._id, name: user.name }, token })
