68d0fa09f540aeb5c1f77d0bcec9d0a24904f4a7
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-10-05 18:23:14 +0530

    added get single job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,13 +1,22 @@
 const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
+const { NotFoundError } = require('../errors')
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId }).sort('-createdAt')
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
-const getJob = (req, res) => {
-  res.send('Get Single Job!')
+const getJob = async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: jobId }
+  } = req
+  const job = await Job.findOne({ _id: jobId, createdBy: userId })
+  if (!job) {
+    throw new NotFoundError(`No job found with id: ${jobId}.`)
+  }
+  res.status(StatusCodes.OK).json(job)
 }
 
 const createJob = async (req, res) => {
