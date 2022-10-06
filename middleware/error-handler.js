20b6b7e74f43428c3bbf52bc7c6e48889d7b179d
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-10-06 23:41:19 +0530

    added mongoose validation  error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,17 @@
 const { StatusCodes } = require('http-status-codes')
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong, Please try again later.'
   }
 
+  if (err.name && err.name === 'ValidationError') {
+    customError.statusCode = 400
+    customError.msg = Object.values(err.errors)
+      .map(item => item.message)
+      .join(', ')
+  }
   if (err.code && err.code === 11000) {
     customError.statusCode = 400
     customError.msg = `Duplicate value entered for ${Object.keys(
