e872fb1e5929941c79f264060e6e1d737e07cf56
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-10-06 23:49:37 +0530

    added mongoose castError handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,13 +7,17 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   }
 
   if (err.name && err.name === 'ValidationError') {
-    customError.statusCode = 400
+    customError.statusCode = StatusCodes.BAD_REQUEST
     customError.msg = Object.values(err.errors)
       .map(item => item.message)
       .join(', ')
   }
+  if (err.name && err.name === 'CastError') {
+    customError.statusCode = StatusCodes.NOT_FOUND
+    customError.msg = `No item found with id: ${err.value}.`
+  }
   if (err.code && err.code === 11000) {
-    customError.statusCode = 400
+    customError.statusCode = StatusCodes.BAD_REQUEST
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`
